ba14c323960b7ebc1f3ef7a423b8faa99a497453
Fe-56 <limfuoen1999@gmail.com>
2022-10-28 22:07:21 +0800

    File Metadata Microservice

--- index.js
+++ index.js
@@ -7,12 +7,21 @@ var app = express();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const file = req.file;
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
